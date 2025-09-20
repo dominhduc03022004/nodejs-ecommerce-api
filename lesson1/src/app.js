3f9d0b455ad8ce22b781ba98fb6374cc00ff9b78
dominhduc03022004 <dominhduc03022004@gmail.com>
2025-09-20 17:32:33 +0700

    them bt nho

--- lesson1/src/app.js
+++ lesson1/src/app.js
@@ -2,6 +2,29 @@ import express from "express";
 
 const app = express();
 
+// Định nghĩa route GET/
+// app = experess()
+// .get : Method HTTP: GET
+// '/': endpoint API - URL
+// function(req, res) :
+// req: du lieu gui tu client(frontend)
+// res: du lieu server tra ve cho client(FE)
+
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
+
+app.get("/api/posts/greet", (req, res) => {
+    const name = req.query.name;
+    res.send(`Xin chào, ${name}!`)
+})
+
+app.get("/api/posts/sum", (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.send(`Tong ${a} + ${b} = ${a+b}`)
+})
+
 app.listen(3000, () => {
   console.log(`Server is running on port http://localhost:3000`);
 });
